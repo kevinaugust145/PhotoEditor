b70cd36a71ddc37acaa225243690b5cd5d826766
KevinHung <kevinaugust145@gmail.com>
2024-08-15 15:28:16 +0800

    Initial Commit

---
+++ PhotoEditor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PhotoEditor
+//
+//  Created by 洪宗燦 on 2024/8/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ PhotoEditorTests/PhotoEditorTests.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoEditorTests.swift
+//  PhotoEditorTests
+//
+//  Created by 洪宗燦 on 2024/8/15.
+//
+
+import Testing
+@testable import PhotoEditor
+
+struct PhotoEditorTests {
+
+    @Test func testExample() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
